14370f19358d71677aa1b4fb58091500ba619111
Fred <frederic.delabre783@gmail.com>
2024-03-27 22:48:21 +0100

    Commit 1

--- src/test/java/Form.java
+++ src/test/java/Form.java
@@ -3,21 +3,30 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Form {
     @Test
     public void t001_setUp(){
-        WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver();
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxDriver driver = new FirefoxDriver();
         driver.get("https://testpages.herokuapp.com/styled/basic-html-form-test.html");
         driver.manage().window().maximize();
-
+        //username
         WebElement username = driver.findElement(By.cssSelector("input[name='username']"));
+        //password
         WebElement password = driver.findElement(By.cssSelector("input[name='password']"));
+        //TextArea Comment
+        WebElement textArea = driver.findElement(By.cssSelector("textarea[name='comments']"));
+
+
 
         username.click();
         username.sendKeys("Udemy");
         password.click();
-        password.sendKeys("Udemy");
+        password.sendKeys("Test@1234");
+        textArea.click();
+        textArea.clear();
+        textArea.sendKeys("Ceci est un commentaire");
     }
 }
