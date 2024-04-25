b9fe78f146d80a6156b1be23795a7b54507d8c38
Fred <frederic.delabre783@gmail.com>
2024-04-25 21:56:08 +0200

    53. Ajout d'un article au panier - Partie 1

--- src/test/java/exSauceDemo/exSauceDemo.java
+++ src/test/java/exSauceDemo/exSauceDemo.java
@@ -1,6 +1,7 @@
 package exSauceDemo;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.bouncycastle.asn1.dvcs.DVCSObjectIdentifiers;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import pageObjectModel.LoginPage;
@@ -9,27 +10,39 @@ import pageObjectModel.ProductPage;
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class exSauceDemo {
     static FirefoxDriver driver;
+
     @BeforeAll
-    public static void setUp(){
+    public static void setUp() {
         WebDriverManager.firefoxdriver().setup();
-        driver= new FirefoxDriver();
+        driver = new FirefoxDriver();
         driver.get("http://saucedemo.com/");
         driver.manage().window().maximize();
     }
+
     @Test
 
-    public void t001_loginUser(){
+    public void t001_loginUser() {
         LoginPage objLoginPage = new LoginPage(driver);
         ProductPage objProductPage = new ProductPage(driver);
         objLoginPage.setLoginAsUser();
         Assertions.assertTrue(objProductPage.getProductPageUrl().contains("inventory.html"));
 
     }
+
     @Test
 
-    public void t002_filterByPrice(){
+    public void t002_filterByPrice() {
         ProductPage objProductPage = new ProductPage(driver);
         objProductPage.setFilterLoHi();
         Assertions.assertTrue(objProductPage.isListInOrder());
+    }
+
+
+    @Test
+
+    public void t003_addArticleToCart() {
+        ProductPage objProductPage = new ProductPage(driver);
+        objProductPage.clickAddToCartBikeLight();
+
     }
 }
--- src/test/java/pageObjectModel/ProductPage.java
+++ src/test/java/pageObjectModel/ProductPage.java
@@ -15,46 +15,57 @@ public class ProductPage {
     //Page Factory
 
     @FindBy(xpath = "//select[@data-test='product-sort-container']")
-            private WebElement filterButton;
+    private WebElement filterButton;
     @FindBy(xpath = "//option[@value='lohi']")
-            private WebElement filterLoHi;
-    @FindBy(xpath ="//div[@class='inventory_item_price']")
-            private List<WebElement> listPrice;
+    private WebElement filterLoHi;
+    @FindBy(xpath = "//div[@class='inventory_item_price']")
+    private List<WebElement> listPrice;
+    @FindBy(id = "add-to-cart-sauce-labs-bike-light")
+    private WebElement addToCartBikeLight;
+    @FindBy(xpath = "//span[@data-test='shopping-cart-badge'")
+    private WebElement cart;
 
-        WebDriver driver;
-        //Constructeur
-        public ProductPage(WebDriver driver){
-            this.driver = driver;
-            PageFactory.initElements(driver, this);
-        }
+    WebDriver driver;
 
-
-        //Page Object
-        public String getProductPageUrl(){
-            return  driver.getCurrentUrl();
-        }
-        public void setFilterLoHi(){
-            filterButton.click();
-            filterLoHi.click();
-        }
-        public Boolean isListInOrder(){
-            //Liste String vide où on va récupérer tous les éléments de notre liste de WebElements
-            List<String> list = new ArrayList<>();
-            //Créer une boucle FOR, permet de lire chaque élément de ListPrice pour les ajouter dans list
-            for(WebElement orderList: listPrice){
-                list.add(orderList.getText().replaceAll("[^\\d]",""));
-            }
-            Iterator<String> iterator = list.iterator();
-            // Convertir le premier élément d'un itérateur en un entier (int)
-            int current, previous = Integer.parseInt(iterator.next());
-            while (iterator.hasNext()){
-                current = Integer.parseInt((iterator.next()));
-                if (previous > current){
-                    return false;
-                }
-                previous = current;
-            };
-            return true;
-
-        }
+    //Constructeur
+    public ProductPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
+
+
+    //Page Object
+    public String getProductPageUrl() {
+        return driver.getCurrentUrl();
+    }
+
+    public void setFilterLoHi() {
+        filterButton.click();
+        filterLoHi.click();
+    }
+
+    public Boolean isListInOrder() {
+        //Liste String vide où on va récupérer tous les éléments de notre liste de WebElements
+        List<String> list = new ArrayList<>();
+        //Créer une boucle FOR, permet de lire chaque élément de ListPrice pour les ajouter dans list
+        for (WebElement orderList : listPrice) {
+            list.add(orderList.getText().replaceAll("[^\\d]", ""));
+        }
+        Iterator<String> iterator = list.iterator();
+        // Convertir le premier élément d'un itérateur en un entier (int)
+        int current, previous = Integer.parseInt(iterator.next());
+        while (iterator.hasNext()) {
+            current = Integer.parseInt((iterator.next()));
+            if (previous > current) {
+                return false;
+            }
+            previous = current;
+        }
+        ;
+        return true;
+    }
+    public void clickAddToCartBikeLight() {
+        addToCartBikeLight.click();
+    }
+
+}
