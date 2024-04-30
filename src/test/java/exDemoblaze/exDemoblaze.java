c62bc222567d8dfed721963ce85e0fd5af3a8466
Fred <frederic.delabre783@gmail.com>
2024-04-30 21:02:01 +0200

    p1

--- src/test/java/exDemoblaze/exDemoblaze.java
+++ src/test/java/exDemoblaze/exDemoblaze.java
@@ -1,4 +1,38 @@
 package exDemoblaze;
 
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import pageObjectModelDemoblaze.Homepage;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
 public class exDemoblaze {
+    static FirefoxDriver driver;
+
+    @BeforeAll
+    public static void setUp() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        driver.get("https://www.demoblaze.com/index.html");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+
+    public void t001_test(){
+        Homepage objHomepage = new Homepage(driver);
+
+        objHomepage.clickCategories();
+        objHomepage.clickPhonesCategory();
+        objHomepage.clickLaptopsCategory();
+        objHomepage.clickMonitorsCategory();
+    }
+
+
+    @AfterAll
+
+    public static void tearDown(){
+        driver.quit();
+    }
 }
---
+++ src/test/java/pageObjectModelDemoblaze/Homepage.java
@@ -0,0 +1,37 @@
+package pageObjectModelDemoblaze;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Homepage {
+    @FindBy(id = "cat")
+            private WebElement categories;
+    @FindBy(xpath = "//a[@id='itemc' and text()='Phones']")
+            private WebElement phonesCategorie;
+    @FindBy(xpath = "//a[@id='itemc' and text()='Laptops']")
+    private WebElement laptopsCategorie;
+    @FindBy(xpath = "//a[@id='itemc' and text()='Monitors']")
+    private WebElement monitorsCategorie;
+
+    WebDriver driver;
+    public Homepage(WebDriver driver){
+        this.driver=driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickCategories(){
+        categories.click();
+    }
+    public void clickPhonesCategory(){
+        phonesCategorie.click();
+    }
+    public void clickLaptopsCategory(){
+        laptopsCategorie.click();
+    }
+    public void clickMonitorsCategory(){
+        monitorsCategorie.click();
+    }
+
+}
