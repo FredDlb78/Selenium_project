95dde25d4b079330c7a61983d8fa3110db1646d1
Fred <frederic.delabre783@gmail.com>
2024-05-01 01:36:49 +0200

    Ajout des derniers éléments et des méthodes pour la HomePage

--- src/test/java/pageObjectModelDemoblaze/HomePage.java
+++ src/test/java/pageObjectModelDemoblaze/HomePage.java
@@ -36,6 +36,18 @@ public class HomePage {
     private WebElement sonyVaioI5;
     @FindBy(xpath = "//a[contains(text(), 'Sony vaio i7')]")
     private WebElement sonyVaioI7;
+    @FindBy(xpath = "//a[contains(text(), 'Apple monitor 24')]")
+    private WebElement appleMonitor24;
+    @FindBy(xpath = "//a[contains(text(), 'MacBook air')]")
+    private WebElement macBookAir;
+    @FindBy(xpath = "//a[contains(text(), 'Dell i7 8gb')]")
+    private WebElement dellI78gb;
+    @FindBy(xpath = "//a[contains(text(), '2017 Dell 15.6 Inch')]")
+    private WebElement dell2017_156Inch;
+    @FindBy(xpath = "//a[contains(text(), 'ASUS Full HD')]")
+    private WebElement asusFullHD;
+    @FindBy(xpath = "//a[contains(text(), 'MacBook Pro')]")
+    private WebElement macBookPro;
     @FindBy(id = "prev2")
             private WebElement previousButton;
     @FindBy(id = "next2")
@@ -49,16 +61,20 @@ public class HomePage {
     }
 
     public void clickCategories(){
-        categories.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(categories)).click();
     }
     public void clickPhonesCategory(){
-        phonesCategorie.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(phonesCategorie)).click();
     }
     public void clickLaptopsCategory(){
-        laptopsCategorie.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(laptopsCategorie)).click();
     }
     public void clickMonitorsCategory(){
-        monitorsCategorie.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(monitorsCategorie)).click();
     }
     public void clickSamsungGalaxyS6(){
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -68,6 +84,59 @@ public class HomePage {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOf(nokiaLumia1520)).click();
     }
+    public void clickNexus6(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(nexus6)).click();
+    }
+    public void clickSamsungGalaxyS7(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(samsungGalaxyS7)).click();
+    }
+    public void clickIPhone632gb(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(iphone632gb)).click();
+    }
+    public void clickSonyXperiaZ5(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(sonyXperiaZ5)).click();
+    }
+    public void clickHtcOneM9(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(htcOneM9)).click();
+    }
+    public void clickSonyVaioI5(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(sonyVaioI5)).click();
+    }
+    public void clickSonyVaioI7(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(sonyVaioI7)).click();
+    }
+    public void clickAppleMonitor24(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(nokiaLumia1520)).click();
+    }
+    public void clickMacBookAir(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(nokiaLumia1520)).click();
+    }
+    public void clickDellI78gb(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(nokiaLumia1520)).click();
+    }
+    public void click2017Dell156Inch(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(nokiaLumia1520)).click();
+    }
+    public void clickAsusFullHD(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(nokiaLumia1520)).click();
+    }
+    public void clickMacBookrPro(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(nokiaLumia1520)).click();
+    }
+
     public void clickPreviousButton(){
         previousButton.click();
     }
