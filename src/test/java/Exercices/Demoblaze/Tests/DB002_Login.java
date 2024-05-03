ed656ec470f5bfc1e6e41d410588f96cdf25ec69
Fred <frederic.delabre783@gmail.com>
2024-05-03 23:37:41 +0200

    Login function tests

--- src/test/java/Exercices/Demoblaze/Tests/DB002_Login.java
+++ src/test/java/Exercices/Demoblaze/Tests/DB002_Login.java
@@ -1,6 +1,5 @@
 package Exercices.Demoblaze.Tests;
 
-import Credentials.Credentials;
 import PageObjectModel.Demoblaze.HeaderPage;
 import PageObjectModel.Demoblaze.HomePage;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -9,12 +8,7 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
-
-import static Exercices.Demoblaze.Tests.DB001_Signin.password;
-import static Exercices.Demoblaze.Tests.DB001_Signin.username;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
@@ -22,11 +16,15 @@ public class DB002_Login {
     static FirefoxDriver driver;
     String username = "JamesSele";
     String password = "Test@1234";
+    String notRegisteredUser = "notRegisterUser1234";
+    String wrongPassword = "WrongPassword";
+    String emptyUsername = "";
+    String emptyPassword = "";
 
 
 
-    @BeforeAll
-    public static void setUp() {
+    @BeforeEach
+    public void setUp() {
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
         driver.get("https://www.demoblaze.com/index.html");
@@ -35,7 +33,7 @@ public class DB002_Login {
     }
     @Test
     @DisplayName("Login01")
-    @Description("")
+    @Description("Passing case")
 
     public void Login01() {
         HeaderPage objHeaderPage = new HeaderPage(driver);
@@ -50,6 +48,74 @@ public class DB002_Login {
         wait.until(ExpectedConditions.visibilityOf(objHeaderPage.getLogoutButton()));
         assertTrue(objHeaderPage.isVisibleLogOutButton());
         assertTrue(objHeaderPage.welcomeUserButtonContainsText(username));
+    }
+    @Test
+    @DisplayName("Login02")
+    @Description("Non-passing case - Not registered user")
+
+    public void Login02() {
+        HeaderPage objHeaderPage = new HeaderPage(driver);
+        HomePage objHomePage = new HomePage(driver);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        assertTrue(objHomePage.getCurrentUrlWeb());
+        objHeaderPage.clickLogInButton();
+        objHeaderPage.setUsernameLoginPopup(notRegisteredUser);
+        objHeaderPage.setPasswordLoginPopup(password);
+        objHeaderPage.clickLogInButtonLogInPopUp();
+        objHeaderPage.verifyAlertText("User does not exist.",
+                "Alert message is not correct");
+    }
+    @Test
+    @DisplayName("Login03")
+    @Description("Non-passing case - Wrong password")
+
+    public void Login03() {
+        HeaderPage objHeaderPage = new HeaderPage(driver);
+        HomePage objHomePage = new HomePage(driver);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        assertTrue(objHomePage.getCurrentUrlWeb());
+        objHeaderPage.clickLogInButton();
+        objHeaderPage.setUsernameLoginPopup(username);
+        objHeaderPage.setPasswordLoginPopup(wrongPassword);
+        objHeaderPage.clickLogInButtonLogInPopUp();
+        objHeaderPage.verifyAlertText("Wrong password.",
+                "Alert message is not correct");
+    }
+    @Test
+    @DisplayName("Login04")
+    @Description("Non-passing case - Empty username")
+
+    public void Login04() {
+        HeaderPage objHeaderPage = new HeaderPage(driver);
+        HomePage objHomePage = new HomePage(driver);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        assertTrue(objHomePage.getCurrentUrlWeb());
+        objHeaderPage.clickLogInButton();
+        objHeaderPage.setUsernameLoginPopup(emptyUsername);
+        objHeaderPage.setPasswordLoginPopup(password);
+        objHeaderPage.clickLogInButtonLogInPopUp();
+        objHeaderPage.verifyAlertText("Please fill out Username and Password.",
+                "Alert message is not correct");
+    }
+    @Test
+    @DisplayName("Login05")
+    @Description("Non-passing case - Empty password")
+
+    public void Login05() {
+        HeaderPage objHeaderPage = new HeaderPage(driver);
+        HomePage objHomePage = new HomePage(driver);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        assertTrue(objHomePage.getCurrentUrlWeb());
+        objHeaderPage.clickLogInButton();
+        objHeaderPage.setUsernameLoginPopup(username);
+        objHeaderPage.setPasswordLoginPopup(emptyPassword);
+        objHeaderPage.clickLogInButtonLogInPopUp();
+        objHeaderPage.verifyAlertText("Please fill out Username and Password.",
+                "Alert message is not correct");
     }
     @AfterEach
 
