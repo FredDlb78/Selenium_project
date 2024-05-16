3de513f51e1ec9f385f748c685bc47fe801ad291
Fred <frederic.delabre783@gmail.com>
2024-05-17 00:09:08 +0200

    Code James

---
+++ src/test/java/Exercices/Demoblaze/Kanban_Jeff/DEM_17.java
@@ -0,0 +1,40 @@
+package Exercices.Demoblaze.Kanban_Jeff;
+
+import PageObjectModel.Demoblaze.HeaderPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import jdk.jfr.Description;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DEM_17 {
+    static FirefoxDriver driver;
+    String contactEmail = "jeffafanou@gmail.com";
+    String contactName = "jeff";
+    String contactMessage = "test12345678910";
+    String contactAlertText = "Thanks for the message!!";
+    @BeforeAll
+    public static void setUp() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        driver.get("https://www.demoblaze.com/index.html");
+        driver.manage().window().maximize();
+    }
+    @Test
+    @DisplayName("DEM-17")
+    @Description("Fill in the contact form")
+
+    public void DEM_17(){
+        HeaderPage objHeaderPage = new HeaderPage(driver);
+
+        objHeaderPage.clickContactButton();
+        Assertions.assertTrue(objHeaderPage.isCorrectContactPopupTitle(), "Wrong message");
+        objHeaderPage.setContactEmailContactPopup(contactEmail);
+        objHeaderPage.setContactNameContactPopup(contactName);
+        objHeaderPage.setMessageContactPopup(contactMessage);
+        objHeaderPage.clickSendMessageButton();
+        objHeaderPage.verifyAlertText(contactAlertText, "Wrong alert message");
+
+    }
+    @AfterAll
+    public static void tearDown(){driver.quit();}
+}
