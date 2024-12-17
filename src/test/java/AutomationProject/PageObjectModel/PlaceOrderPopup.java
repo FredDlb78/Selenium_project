5f277c94e35cab4f3b228778a0b682a59d7d3107
JamesGit <habricot.james@gmail.com>
2024-12-17 15:15:25 +0100

    E2E

---
+++ src/test/java/AutomationProject/PageObjectModel/PlaceOrderPopup.java
@@ -0,0 +1,24 @@
+package AutomationProject.PageObjectModel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class PlaceOrderPopup {
+        private WebDriver driver;
+        private WebDriverWait wait;
+
+        public PlaceOrderPopup(WebDriver driver) {
+            this.driver = driver;
+            this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            PageFactory.initElements(driver, this);
+        }
+
+        @FindBy(id = "name")
+
+
+
+}
