6c35f20118dbdfaeb042809f4ac1ec3e53896fa2
JamesGit <habricot.james@gmail.com>
2024-12-30 16:44:29 +0100

    ajout d'un 3e article et de la suppression d'un article par position dans la liste

--- src/test/java/AutomationProject/PageObjectModel/CartPage.java
+++ src/test/java/AutomationProject/PageObjectModel/CartPage.java
@@ -1,9 +1,12 @@
 package AutomationProject.PageObjectModel;
 
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -25,6 +28,13 @@ public class CartPage {
         placeOrderButton.click();
         return new PlaceOrderPopup(driver);
     }
-
+    @Step("Click on delete button")
+    public CartPage clickDelete(Integer index) {
+        String xpath = String.format("(//a[contains(text(),'Delete')])[%d]", index);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement deleteButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+        deleteButton.click();
+        return this;
+    }
 
 }
